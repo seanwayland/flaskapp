87593b3c025d249d29318619fad8d3b3dbdca0dc
seanwayland <seanwayland@gmail.com>
2022-12-17 21:56:09 -0500

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# two decorators, same function
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html', the_title='Sean Wayland Music')
+
+@app.route('/myth.html')
+def myth():
+    return render_template('myth.html', the_title='Tiger in Myth and Legend')
+
+if __name__ == '__main__':
+    app.run(debug=True)
