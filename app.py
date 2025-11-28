7a67eb595c82d5c8a1c82a6da913c66af9bdadbd
seanwayland <seanwayland@gmail.com>
2025-11-27 22:49:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=access_key_id,
     aws_secret_access_key=secret_access_key,
-    region_name="us-east-2"
+    region_name="us-east-1"
 )
 
 AWS_S3_SIGNATURE_VERSION = "s3v4"
